23d3e72da1c1d497be528410f15fb7d4ee26946c
DrDrifter <drifter@netti.fi>
2018-04-24 16:21:31 +0300

    Added Hair's autocruise

--- bat-sailor.tf
+++ bat-sailor.tf
@@ -73,3 +73,78 @@
 
 /DEF -mregexp -F -i -t"^This disc is really a massive ring" ship_disc10_sub = \
 	/ECHO -p @{BCwhite}Disc 10@{n}
+
+
+;
+; AutoCruise by Hair
+;
+/DEF -i -ag -h"send {autocruise}*" ship_auto_cruise = \
+	/IF (regmatch("^([0-9]*)x([0-9]*)y$", %{-1} ) ) \
+		/SET ship_autocruise=1 %; \
+		/SET ac_flag=1 %; \
+		/SET ac_destx=%{P1} %; \
+		/SET ac_desty=%{P2} %; \
+		/SEND @whereami %; \
+	/ELSE /ECHO -aCbgyellow,Cblack *** Usage:  autocruise 123x123y *** %; \
+	/ENDIF
+
+/DEF -i -mregexp -ag -t"^You are in (.*) on the continent of ([A-Za-z]*). \(Coordinates\: ([0-9]*)x, ([0-9]*)y\; Global\: ([0-9]*)x, ([0-9]*)y\)$" get_xy_coordinates = \
+	/IF (%{ship_autocruise} == 1) \
+		/SET ship_xcoord=%{P3} %; \
+		/SET ship_ycoord=%{P4} %; \
+		/do_auto_cruise %; \
+	/ELSE /ECHO %{P0} %; \
+	/ENDIF
+
+/DEF -i do_auto_cruise = \
+	/SET ship_autocruise=0 %; \
+	/LET ac_xdiff=$[%{ac_destx}-%{ship_xcoord}] %; \
+	/LET ac_ydiff=$[%{ac_desty}-%{ship_ycoord}] %; \
+	/IF (%{ac_xdiff} < 0) /SET ac_xdir=west %; \
+	/ELSEIF (%{ac_xdiff} > 0) /SET ac_xdir=east %; \
+	/ELSEIF (%{ac_xdiff} == 0) /SET ac_xdir=nil %; \
+	/ENDIF %; \
+	/IF (%{ac_ydiff} < 0) /SET ac_ydir=north %; \
+	/ELSEIF (%{ac_ydiff} > 0) /SET ac_ydir=south %; \
+	/ELSEIF (%{ac_ydiff} == 0) /SET ac_ydir=nil %; \
+	/ENDIF %; \
+	/LET ac_xdiff=$[abs(%{ac_xdiff})] %; \
+	/LET ac_ydiff=$[abs(%{ac_ydiff})] %; \
+	/IF (%{ac_xdir} =~ "nil") \
+		/IF (%{ac_ydir} =~ "nil") \
+			/SET ship_autocruise=0 %; \
+		/ELSE /SEND @cruise %{ac_ydiff} %{ac_ydir} %; \
+		/ENDIF %; \
+	/ELSEIF (%{ac_ydir} =~ "nil") \
+		/IF (%{ac_xdir} =~ "nil") \
+			/SET ship_autocruise=0 %; \
+		/ELSE /SEND @cruise %{ac_xdiff} %{ac_xdir} %; \
+		/ENDIF %; \
+	/ELSEIF (%{ac_xdiff} > %{ac_ydiff}) \
+		/SET ship_autocruise=1 %; \
+		/SEND @cruise %{ac_ydiff} %{ac_ydir}%{ac_xdir} %; \
+	/ELSEIF (%{ac_ydiff} > %{ac_xdiff}) \
+		/SET ship_autocruise=1 %; \
+		/SEND @cruise %{ac_xdiff} %{ac_ydir}%{ac_xdir} %; \
+	/ENDIF
+
+/DEF -i -mregexp -ag -t"(.*) tells you \'We\'ve gon\' as far towards ([a-z]*) as ask\'d, (.*).\'$" continue_autocruise = \
+	/IF (%{ship_autocruise} == 1) \
+		/SEND @whereami %; \
+	/ELSE /ECHO %{P0} %; \
+	/ENDIF %; \
+	/IF (%{ac_flag} == 1 & %{ship_autocruise} == 0) \
+		/ECHO -aCbgyellow,Cblack *** Autocruise Complete *** %; \
+		/SET ac_flag=0 %; \
+	/ENDIF
+
+/DEF -i -mregexp -t"(.*) whispers to you \'I din\'nae think we\'ll get verra far wit\' tha anchor down.\'$" ac_oops_anchored = \
+	/IF (%{ship_autocruise} == 1) \
+		/SEND @ship launch %; \
+		/SEND @whereami %; \
+	/ENDIF
+
+/DEF -i -mregexp -t"(.*) tells you \'We\'ve can\'nae go ena more towards (.*).\'$" ac_terrain_block = \
+	/SET ship_autocruise=0 %; \
+	/SET ac_flag=0 %; \
+	/ECHO -aCbgyellow,Cblack *** Autocruise Aborted.  Cannot clear terrain ***
