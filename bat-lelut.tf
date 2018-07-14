d5a8f70d9397787ada30570d4781e13d54feb6bb
DrDrifter <drifter@netti.fi>
2018-07-14 13:36:23 +0300

    Small tweaks to lelut.tf
    Changed some cooldowns etc

--- bat-lelut.tf
+++ bat-lelut.tf
@@ -17,31 +17,37 @@
 /set nithem_skull_status=X
 /set pfe_helmet_status=X
 /set bra_status=_
+/set demonring_status=X
+/set wlizard_ring_1_status=X
+/set wlizard_ring_2_status=X
 /set fqueen_wand_status=X
 
 ;; Athame
-/def -t"The athame severs the field surrounding this area." athamesever = @remove Leikkuri;put Leikkuri in bp;/set athame_status=_%;/set athame_time=$[time()]%;/repeat -00:10 1 /set athame_status=X
+;; Assuming 10 minute cooldown
+/def -t"The athame severs the field surrounding this area." athamesever=@remove Leikkuri;put Leikkuri in bp;/set athame_status=_%;/set athame_time=$[time()]%;/repeat -00:10 1 /set athame_status=X
 
 ;; Rain Staff
-/def -mglob -t"You strike your rain staff in the ground." strikestaff = /set rainstaff_status=_%;/set rainstaff_time=$[time()]%;/repeat -00:10 1 /set rainstaff_status=X
+;; Assuming 10 minute cooldown
+/def -mglob -t"You strike your rain staff in the ground." strikestaff=/set rainstaff_status=_%;/set rainstaff_time=$[time()]%;/repeat -00:10 1 /set rainstaff_status=X
 
 ;; Entity drying wind
-/def -t'Nagato the magic entity holds the Shield of the Wind up high!' magic_entity_wind_timer = /set ewind_status=_%;/set ewind_time=$[time()]%;/repeat -00:30 1 /set ewind_status=X
+;; Needs update, have 2 shields
+/def -t'Nagato the magic entity holds the Shield of the Wind up high!' magic_entity_wind_timer=/set ewind_status=_%;/set ewind_time=$[time()]%;/repeat -00:30 1 /set ewind_status=X
 
 ;; Mage staff
-/def -mglob -t"You invoke the powers of your mage staff." invoke_magestaff = /set magestaff_status=_%;/set magestaff_time=$[time()]%;/repeat -00:5 /set magestaff_status=X
+/def -mglob -t"You invoke the powers of your mage staff." invoke_magestaff=/set magestaff_status=_%;/set magestaff_time=$[time()]%;/repeat -00:5 1 /set magestaff_status=X
 
 ;; Orb of wind and rain
-/def -mglob -t"Your orb sparkles." orbready = /set orb_status=X
+/def -mglob -t"Your orb sparkles." orbready=/set orb_status=X
 
 ;; Ankh
-/def -mglob -t"Your Amberley Ankh activates." ankh_activated = /set ankh_status=_%;/set ankh_time=$[time()]%;/repeat -00:30 1 /set ankh_status=X
+/def -mglob -t"Your Amberley Ankh activates." ankh_activated=/set ankh_status=_%;/set ankh_time=$[time()]%;/repeat -00:30 1 /set ankh_status=X
 
 ;; Henry plate
-/def -mglob -t"You feel magical powers flowing from it back to you." henry_plate_charged = /set henry_plate_amount = %henry_plate_amount-50
+/def -mglob -t"You feel magical powers flowing from it back to you." henry_plate_charged=/set henry_plate_amount=%henry_plate_amount - 50
 /def -mglob -t"You feel magical powers flowing from you to the breastplate." henry_plate_drained = \
    /set henry_plate_status=X%;\
-   /set henry_plate_amount=%henry_plate_amount+50
+   /set henry_plate_amount= %henry_plate_amount + 50
 /def -mglob -t"But the plate is without response." henry_plate_empty = /set henry_plate_status=_
 
 ;; Yaboz bracelets
@@ -56,6 +62,7 @@
 ;; Note: Cooldown is actually 15-25 minutes!
 /def -mglob -t"The spirit of Morrigaine appears in your visions and grants you protection." pfe_helmet_used =\
    /set pfe_helmet_status=_%;\
+   /set pfe_helmet_time=$[time()]%;\
    /repeat -0:20 1 /set pfe_helmet_status=X
 
 ;; Fairy bra
@@ -71,19 +78,21 @@
 ;; Mask of Gluttony
 
 ;; Demonic ring
-/def -F -mglob -t'You wear Demonic Ring of Invisibility*' demonring_wear = /repeat -0:10 1 /set 
+/def -F -mglob -t'You wear Demonic Ring of Invisibility*' demonring_wear = /set demonring_status=_%;/repeat -0:10 1 /set demonring_status=X
 
 ;; Werelizard rings
-/def -mglob -t"You feel strength flowing between you and *" ringkiss = /repeat -00:10 1 @party report (Ring kiss loaded)
+;;You wear a golden ring with a single small ruby labeled as Fritsuli (glowing) <purple glow>. -> ring 1
+;;You wear a golden ring with a single small ruby labeled as Kielari (glowing) <purple glow>.  -> ring 2
+/def -mglob -t"You feel strength flowing between you and *" ringkiss = /repeat -00:10 1 /ECHO (TF Info): Ring kiss loaded
 
 ;; Fqueen wand
-/def -mlob -t"You swing the wand one last time, pointing at *" fqueen_wand_zapped = /set fqueen_wand_status=_
+/def -mglob -t"You swing the wand one last time, pointing at *" fqueen_wand_zapped = /set fqueen_wand_status=_
 
 /def lelut=\
 /echo  .-----------------------------.%;\
 /echo  |   Drifter's tweak eq status |%;\
 /echo  +-----------------------------+%;\
-/echo  | Fields:
+/echo  | Fields:%;\
 /echo  |  Athame (asev) [%athame_status] [%athame_cd]%;\
 /echo  |  Rainstaff (rain) [%rainstaff_status] [%rainstaff_cd]%;\
 /echo  |  Entity Wind (entw,ewind) [%ewind_status] [%ewind_cd] %;\
@@ -105,7 +114,7 @@
 /echo  | Other: %;\
 /echo  |  Mask of Gluttony (wispill) [%mask_of_gluttony_status] [%mask_of_gluttony_cd]%;\
 /echo  |  Demonic Ring [%demonic_ring_status] [%demonic_ring_cd]%;\
-/echo  |  Lizard ring1 []%;\
-/echo  |  Lizard ring2 []%;\
+/echo  |  Lizard ring1 (rkiss)  []%;\
+/echo  |  Lizard ring2 (rkiss2) []%;\
 /echo  |  Fairy wand (frest) [%fqueen_wand_status] [%fqueen_wand_cd]%;\
 /echo  `-----------------------------`
