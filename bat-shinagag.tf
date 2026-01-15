b333f2525c52f24505d50588f8ffa3e03855a031
DrDrifter <andrey.khasparov@protonmail.com>
2026-01-15 07:13:28 +0200

    Added old shinarae event gag triggers

---
+++ bat-shinagag.tf
@@ -0,0 +1,8 @@
+/def -ag -mglob -t"Foreman Dorbo shouts*" shinaeventgag1
+/def -ag -mglob -t"Lordrei Minir shouts*" shinaeventgag2
+/def -ag -mglob -t"Elder Serpent shouts*" shinaeventgag3
+/def -ag -mglob -t"Sgt. Linirae shouts*" shinaeventgag4
+/def -ag -mglob -t"Diplomat Cherdrar shouts*" shinaeventgag5
+/def -ag -mglob -t"Tyrannosaurus rex shouts*" shinaeventgag6
+
+/def -ag -mglob -t"* shipment handed over to*" shinaeventgag10
